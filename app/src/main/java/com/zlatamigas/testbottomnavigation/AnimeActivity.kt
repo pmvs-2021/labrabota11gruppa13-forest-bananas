a76da177bb0734224834a20aa19f452820b76f61
Zlata Migas <zlatamigas@gmail.com>
2021-12-28 13:56:30 +0300

    calendar and button,navigation

--- app/src/main/java/com/zlatamigas/testbottomnavigation/AnimeActivity.kt
+++ app/src/main/java/com/zlatamigas/testbottomnavigation/AnimeActivity.kt
@@ -1,12 +1,66 @@
 package com.zlatamigas.testbottomnavigation
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class AnimeActivity: AppCompatActivity() {
+class AnimeActivity : AppCompatActivity() {
+
+    lateinit var idIVAddFavourite: ImageView
+    lateinit var idIVAddNotification: ImageView
+
+    lateinit var idIVAnimeCover: ImageView
+    lateinit var idTVAnimeTitle: TextView
+    lateinit var idTVAnimeRating: TextView
+    lateinit var idTVAnimeYears: TextView
+    lateinit var idTVAnimeEpisodes: TextView
+    lateinit var idTVAnimeAge: TextView
+    lateinit var idTVAnimeGenres: TextView
+    lateinit var idTVAnimeDescription: TextView
+
+    var isFavourite = false
+    var isNotified = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anime)
+
+        //Receive data from bundle about depicted element
+
+        idIVAddFavourite = findViewById(R.id.idIVAddFavourite)
+        idIVAddNotification = findViewById(R.id.idIVAddNotification)
+        idIVAnimeCover = findViewById(R.id.idIVAnimeCover)
+
+        idTVAnimeTitle = findViewById(R.id.idTVAnimeTitle)
+        idTVAnimeRating = findViewById(R.id.idTVAnimeRating)
+        idTVAnimeYears = findViewById(R.id.idTVAnimeYears)
+        idTVAnimeEpisodes = findViewById(R.id.idTVAnimeEpisodes)
+        idTVAnimeAge = findViewById(R.id.idTVAnimeAge)
+        idTVAnimeGenres = findViewById(R.id.idTVAnimeGenres)
+        idTVAnimeDescription = findViewById(R.id.idTVAnimeDescription)
+
+        idIVAddFavourite.setOnClickListener {
+            idIVAddFavourite.setImageResource(
+                if (isFavourite) {
+                    R.drawable.ic_star_not_selected
+                } else {
+                    R.drawable.ic_star_selected
+                }
+            )
+            isFavourite = !isFavourite
+        }
+
+        idIVAddNotification.setOnClickListener {
+            idIVAddNotification.setImageResource(
+                if (isNotified) {
+                    R.drawable.ic_notify_off
+                } else {
+                    R.drawable.ic_notify_on
+                }
+            )
+            isNotified = !isNotified
+        }
     }
 
     override fun onBackPressed() {
