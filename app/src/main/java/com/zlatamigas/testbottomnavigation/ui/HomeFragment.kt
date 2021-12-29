068786dca9bb62502f3c50a35da1469d19c3b2f5
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2021-12-29 12:01:08 +0300

    filtering function expanded

--- app/src/main/java/com/zlatamigas/testbottomnavigation/ui/HomeFragment.kt
+++ app/src/main/java/com/zlatamigas/testbottomnavigation/ui/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.zlatamigas.testbottomnavigation.ui
 
+import android.R
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +21,12 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.ArrayList
+import android.widget.RadioButton
+
+import android.widget.RadioGroup
+
+
+
 
 
 class HomeFragment : Fragment() {
@@ -34,7 +42,7 @@ class HomeFragment : Fragment() {
 
     lateinit var animeRVModalArrayList: ArrayList<AnimeRVModal>
     lateinit var animeRVAdapter: AnimeRVAdapter
-    lateinit var grid: GridLayout
+    lateinit var linear: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,8 +59,18 @@ class HomeFragment : Fragment() {
         idPBLoadSearchResults = binding.idPBLoadSearchResults
         idIVSearch = binding.idIVSearch
         idTIESearch = binding.idTIESearch
-        grid = binding.grid
+        linear = binding.linear
+
+        var filterBy = ArrayList<String>()
+        var filters = ArrayList<String>()
+        var sortBy: String? = null
+
+        var grid = GridLayout(activity)
         grid.columnCount = 4
+
+        var lable = TextView(activity)
+        lable.text = "Genres:"
+        linear.addView(lable)
 
         val genresCheckboxes = ArrayList<CheckBox>()
         var check = CheckBox(activity)
@@ -68,13 +86,60 @@ class HomeFragment : Fragment() {
         grid.addView(check)
         genresCheckboxes.add(check)
         check = CheckBox(activity)
-        check.text = "Fantasy"
+        check.text = "Action"
         grid.addView(check)
         genresCheckboxes.add(check)
         check = CheckBox(activity)
         check.text = "Horror"
         grid.addView(check)
         genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "Drama"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "Magic"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "Supernatural"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "Fantasy"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "Sports"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "Romance"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "Music"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "Thriller"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "School"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        check = CheckBox(activity)
+        check.text = "Historical"
+        grid.addView(check)
+        genresCheckboxes.add(check)
+        linear.addView(grid)
+
+        grid = GridLayout(activity)
+        grid.columnCount = 4
+        lable = TextView(activity)
+        lable.text = "Age ratings:"
+        linear.addView(lable)
 
         val agesCheckboxes = ArrayList<CheckBox>()
         check = CheckBox(activity)
@@ -93,14 +158,14 @@ class HomeFragment : Fragment() {
         check.text = "R18"
         grid.addView(check)
         agesCheckboxes.add(check)
+        linear.addView(grid)
 
         animeRVModalArrayList = ArrayList()
         animeRVAdapter = AnimeRVAdapter(requireActivity(), animeRVModalArrayList!!)
 
         idRVAnimeListFound.setAdapter(animeRVAdapter)
 
-        var filterBy = ArrayList<String>()
-        var filters = ArrayList<String>()
+
 
         idRVAnimeListFound.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
@@ -109,7 +174,7 @@ class HomeFragment : Fragment() {
                     page++
                     GlobalScope.launch {
                         val animes = controller?.getAnimes(idTIESearch.text.toString(),
-                            null, filterBy, filters, page)
+                            sortBy, filterBy, filters, page)
                         withContext(Dispatchers.Main) {
                             if (animes != null) {
                                 for (anime in animes) {
@@ -137,63 +202,57 @@ class HomeFragment : Fragment() {
 
             filterBy = ArrayList<String>()
             filters = ArrayList<String>()
-            
-            if (!str.isEmpty()) {
-                idRVAnimeListFound.visibility = View.GONE
-                idPBLoadSearchResults.visibility = View.VISIBLE
-                var genres = ""
-                var ages = ""
-                val checkedGenres = genresCheckboxes.filter { cb -> cb.isChecked }
-                val checkedAges = agesCheckboxes.filter { cb -> cb.isChecked }
-                if (!checkedGenres.isNullOrEmpty()) {
-                    filterBy.add("genres")
-                    genres = checkedGenres[0].text.toString()
-                    for (i in 1 until checkedGenres.size) {
-                        genres += ",${checkedGenres[i].text}"
-                    }
-                    filters.add(genres)
-                }
 
-                if (!checkedAges.isNullOrEmpty()) {
-                    filterBy.add("ageRating")
-                    ages = checkedAges[0].text.toString()
-                    for (i in 1 until checkedAges.size) {
-                        ages += ",${checkedAges[i].text}"
-                    }
-                    filters.add(ages)
+            idRVAnimeListFound.visibility = View.GONE
+            idPBLoadSearchResults.visibility = View.VISIBLE
+            var genres = ""
+            var ages = ""
+            val checkedGenres = genresCheckboxes.filter { cb -> cb.isChecked }
+            val checkedAges = agesCheckboxes.filter { cb -> cb.isChecked }
+            if (!checkedGenres.isNullOrEmpty()) {
+                filterBy.add("genres")
+                genres = checkedGenres[0].text.toString()
+                for (i in 1 until checkedGenres.size) {
+                    genres += ",${checkedGenres[i].text}"
                 }
-
-                page = 0
-                GlobalScope.launch {
-                    val animes = controller?.getAnimes(
-                        idTIESearch.text.toString(),
-                        null, filterBy, filters, page
-                    )
-                    withContext(Dispatchers.Main) {
-                        animeRVModalArrayList.clear()
-                        if (animes != null) {
-                            for (anime in animes) {
-                                animeRVModalArrayList.add(
-                                    AnimeRVModal(
-                                        anime.id,
-                                        checkNullString(anime.title),
-                                        checkNullString(anime.rating),
-                                        checkNullString(anime.episodeCount),
-                                        anime.posterImage
-                                    )
-                                )
-                            }
-                        }
-
-                        animeRVAdapter.notifyDataSetChanged()
-                        idRVAnimeListFound.visibility = View.VISIBLE
-                        idPBLoadSearchResults.visibility = View.GONE
-                    }
-                }
+                filters.add(genres)
             }
-            else {
-                Toast.makeText(requireActivity(), "Please enter Anime Title", Toast.LENGTH_SHORT)
-                    .show()
+
+            if (!checkedAges.isNullOrEmpty()) {
+                filterBy.add("ageRating")
+                ages = checkedAges[0].text.toString()
+                for (i in 1 until checkedAges.size) {
+                    ages += ",${checkedAges[i].text}"
+                }
+                filters.add(ages)
+            }
+
+            page = 0
+            GlobalScope.launch {
+                val animes = controller?.getAnimes(
+                    idTIESearch.text.toString(),
+                    sortBy, filterBy, filters, page
+                )
+                withContext(Dispatchers.Main) {
+                    animeRVModalArrayList.clear()
+                    if (animes != null) {
+                        for (anime in animes) {
+                            animeRVModalArrayList.add(
+                                AnimeRVModal(
+                                    anime.id,
+                                    checkNullString(anime.title),
+                                    checkNullString(anime.rating),
+                                    checkNullString(anime.episodeCount),
+                                    anime.posterImage
+                                )
+                            )
+                        }
+                    }
+
+                    animeRVAdapter.notifyDataSetChanged()
+                    idRVAnimeListFound.visibility = View.VISIBLE
+                    idPBLoadSearchResults.visibility = View.GONE
+                }
             }
         })
 
